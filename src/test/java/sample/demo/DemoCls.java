ecf31691845c38c1cc797d72b2028ebc79d56917
Anji <Anji@DESKTOP-ALDGQLE>
2023-04-12 11:38:52 +0530

    adding changes into demo committed

--- src/test/java/sample/demo/DemoCls.java
+++ src/test/java/sample/demo/DemoCls.java
@@ -4,9 +4,12 @@ import org.testng.annotations.Test;
 
 public class DemoCls {
 	@Test
-	public void dest()
-	{
+	public void dest() {
 		System.out.println("i am in demo ");
 	}
 
+	@Test
+	public void demo() {
+		System.out.println("we are secure for the local repo");
+	}
 }
